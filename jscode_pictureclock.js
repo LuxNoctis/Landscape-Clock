c51d4c8014afebaac1cf0ecc6c9ee717419b0e68
LuxNoctis <47633546+LuxNoctis@users.noreply.github.com>
2019-05-02 13:03:34 +0100

    made changes

--- jscode_pictureclock.js
+++ jscode_pictureclock.js
@@ -51,7 +51,7 @@ var updateClock = function()
 {
   var time = new Date().getHours();
   var messageText;
-  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+  var image = "https://www.braverlaw.net/wp-content/uploads/2016/04/sunny-day.jpg";
 
   var timeEventJS = document.getElementById("timeEvent");
   var lolcatImageJS = document.getElementById('lolcatImage');
@@ -92,7 +92,7 @@ updateClock();
 
 // Getting the clock to increment once a second
 var oneSecond = 1000;
-setInterval( updateClock, oneSecond);
+setInterval(updateClock, oneSecond);
 
 
 // party time button stuff
